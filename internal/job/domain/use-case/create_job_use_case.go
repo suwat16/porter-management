eca00fc7f5e1d7143a945624a2f516d7adb224c2
suwat16 <suwat16128@gmail.com>
2024-01-21 13:10:15 +0700

    refactor: 💡 use createNewJob in excute

--- internal/job/domain/use-case/create_job_use_case.go
+++ internal/job/domain/use-case/create_job_use_case.go
@@ -6,10 +6,16 @@ type CreateJobUseCase struct {
 	JobRepository domain.JobRepository
 }
 
-func (c *CreateJobUseCase) Execute(job *domain.Job) (*domain.Job, error) {
-	err := c.JobRepository.Save(job)
+func (c *CreateJobUseCase) Execute(jobName string, requester domain.Requester, destination domain.Destination) (*domain.Job, error) {
+	job, err := domain.CreateNewJob(jobName, requester, destination)
 	if err != nil {
 		return nil, err
 	}
-	return job, nil
+
+	err = c.JobRepository.Save(&job)
+	if err != nil {
+		return nil, err
+	}
+
+	return &job, nil
 }
