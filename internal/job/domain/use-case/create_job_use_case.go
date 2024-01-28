ff7c0f4037cadcc7d8e224d30b4b8576cb23fcc2
suwat16 <suwat16128@gmail.com>
2024-01-28 11:36:27 +0700

    refactor: 💡 save job to db

--- internal/job/domain/use-case/create_job_use_case.go
+++ internal/job/domain/use-case/create_job_use_case.go
@@ -1,16 +1,21 @@
 package usecase
 
 import (
-	infra "porter-management/internal/job-request/infra/repository"
 	"porter-management/internal/job/domain/entity"
+	"porter-management/internal/job/infra/repository"
 )
 
 type CreateJobUseCase struct {
-	JobRepository infra.JobRequestRepo
+	JobRepository repository.JobRepository
 }
 
 func (c *CreateJobUseCase) Execute(jobName string, requester entity.Requester, destination entity.Destination) (*entity.Job, error) {
 	job, err := entity.CreateNewJob(jobName, requester, destination)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = c.JobRepository.Save(&job)
 	if err != nil {
 		return nil, err
 	}
