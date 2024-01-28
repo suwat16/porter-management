679d595ea980130f234ffffc5ee02a8ae6fc6d47
suwat16 <suwat16128@gmail.com>
2024-01-28 14:22:41 +0700

    test: 💍 create job repo test file

---
+++ internal/job/infra/repository/job_repository_test.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"porter-management/internal/job/domain/entity"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+)
+
+func TestSave(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	tx, _ := db.Begin()
+	repo := NewJobRepository(tx)
+
+	jobName := "job name"
+
+	requester := entity.Requester{
+		Name:     "requester name",
+		Position: "requester position",
+	}
+
+	destination := entity.Destination{
+		Building: "destination building",
+		Floor:    "destination floor",
+		Room:     "destination room",
+	}
+
+	job, err := entity.CreateNewJob(jobName, requester, destination)
+
+	mock.ExpectExec("INSERT INTO jobs").
+		WithArgs(job.Id, job.Name, job.Requester.Name, job.Requester.Position, job.Destination.Building, job.Destination.Floor, job.Destination.Room, job.Status).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	_, err = repo.Save(&job)
+	if err != nil {
+		t.Errorf("error was not expected while saving job: %s", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
