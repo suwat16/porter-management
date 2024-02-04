4f3e92627d7eeda157475555b8c9b7c34482471a
suwat16 <suwat16128@gmail.com>
2024-02-04 11:35:19 +0700

    refactor: 💡 remove controller and db

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"porter-management/config"
-	controller "porter-management/internal/job/application"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	config.InitDb()
-
 	route := gin.Default()
-	controller.InitJobController(route)
 
 	route.Run(":8080")
 }
