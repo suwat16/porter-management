d1c78f50c4d40de59046672764b84c33cbf69e50
suwat16 <suwat16128@gmail.com>
2024-02-04 12:30:47 +0700

    refactor: 💡 rename of file and folder

--- internal/job/domain/use_case/job_use_case.go
+++ internal/job/domain/use_case/job_use_case.go
@@ -6,18 +6,24 @@ import (
 	"porter-management/internal/job/infra/repository"
 )
 
-type CreateJobUseCase struct {
+type JobUseCase struct {
 	JobRepository repository.JobRepository
 }
 
-func (c *CreateJobUseCase) Execute(uow config.Uow, jobName string, requester entity.Requester, destination entity.Destination, equipment entity.Equipment) (*entity.Job, error) {
+func NewJobUseCase(jobRepository repository.JobRepository) *JobUseCase {
+	return &JobUseCase{
+		JobRepository: jobRepository,
+	}
+}
+
+func (jobUseCase *JobUseCase) ExecuteNewJob(uow config.Uow, jobName string, requester entity.Requester, destination entity.Destination, equipment entity.Equipment) (*entity.Job, error) {
 	job, err := entity.CreateNewJob(jobName, requester, destination, equipment)
 	if err != nil {
 		return nil, err
 	}
 
 	err = uow.DoInTransaction(func() error {
-		_, err := c.JobRepository.Save(&job)
+		_, err := jobUseCase.JobRepository.Save(&job)
 		if err != nil {
 			return err
 		}
