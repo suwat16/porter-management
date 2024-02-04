8aeab215b14a7d410feefe685db354c96431d38c
suwat16 <suwat16128@gmail.com>
2024-02-04 11:36:28 +0700

    refactor: 💡 pass tx in job repo save

--- internal/job/infra/repository/job_repository.go
+++ internal/job/infra/repository/job_repository.go
@@ -6,15 +6,15 @@ import (
 )
 
 type JobRepository struct {
-	db *sql.Tx
+	tx *sql.Tx
 }
 
-func NewJobRepository(db *sql.Tx) *JobRepository {
-	return &JobRepository{db: db}
+func NewJobRepository(tx *sql.Tx) *JobRepository {
+	return &JobRepository{tx: tx}
 }
 
 func (repo *JobRepository) Save(job *entity.Job) (entity.Job, error) {
-	_, err := repo.db.Exec("INSERT INTO jobs (id, version, name, status, requester_name, requester_position, destination_building, destination_floor, destination_room) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", job.Id, job.Version, job.Name, job.Status, job.Requester.Name, job.Requester.Position, job.Destination.Building, job.Destination.Floor, job.Destination.Room)
+	_, err := repo.tx.Exec("INSERT INTO jobs (id, version, name, status, requester_name, requester_position, destination_building, destination_floor, destination_room) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", job.Id, job.Version, job.Name, job.Status, job.Requester.Name, job.Requester.Position, job.Destination.Building, job.Destination.Floor, job.Destination.Room)
 	if err != nil {
 		return entity.Job{}, err
 	}
