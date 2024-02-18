8123245cf6aabb285ce0febd25e148c443db0c82
suwat16 <suwat16128@gmail.com>
2024-02-18 14:32:33 +0700

    feat: 🎸 init and connect amqp

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/streadway/amqp"
+
+	jobC "porter-management/internal/job/application"
 )
 
 func main() {
 	route := gin.Default()
+
+	// Connect to RabbitMQ
+	amqp, err := amqp.Dial("amqp://rabbitmq:password@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	defer amqp.Close()
+
+	// Create a channel
+	ch, err := amqp.Channel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	jobCTRLRegis := jobC.NewJobController(route, ch)
+	jobCTRLRegis.RegisterRoutes()
 
 	route.Run(":8080")
 }
--- internal/job/application/job_controller.go
+++ internal/job/application/job_controller.go
@@ -1,18 +1,25 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/streadway/amqp"
+)
 
 type JobController struct {
 	gin *gin.Engine
+	ch  *amqp.Channel
 }
 
-func NewJobController(gin *gin.Engine) *JobController {
-	return &JobController{gin: gin}
+func NewJobController(gin *gin.Engine, ch *amqp.Channel) *JobController {
+	return &JobController{gin: gin, ch: ch}
 }
 
-func (jobController *JobController) CreateNewJob(c *gin.Context) {
+func (jobController *JobController) RegisterRoutes() {
+	jobController.gin.GET("/job", jobController.GetJobs)
+}
+
+func (jobController *JobController) GetJobs(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
-	// ...
 }
