2ee1bafa271fb984ece0fb186c573536cffbfd85
suwat16 <suwat16128@gmail.com>
2024-02-18 13:29:11 +0700

    refactor: 💡 renew file controller

--- internal/job/application/job_controller.go
+++ internal/job/application/job_controller.go
@@ -1,48 +1,18 @@
 package controller
 
-import (
-	"porter-management/config"
-	entity "porter-management/internal/job/domain/entity"
-	usecase "porter-management/internal/job/domain/use_case"
-
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
 
 type JobController struct {
-	JobUseCase usecase.JobUseCase
-	Uow        config.Uow
+	gin *gin.Engine
 }
 
-func NewJobController(jobUseCase usecase.JobUseCase, uow config.Uow) *JobController {
-	return &JobController{
-		JobUseCase: jobUseCase,
-		Uow:        uow,
-	}
+func NewJobController(gin *gin.Engine) *JobController {
+	return &JobController{gin: gin}
 }
 
-func (jobC *JobController) InitController(router *gin.Engine) {
-	JobController := router.Group("/job")
-	{
-		JobController.POST("/new")
-	}
-}
-
-func (jobC *JobController) NewJob(c *gin.Context) {
-
-	jobName := c.PostForm("jobName")
-
-	requester := entity.Requester{
-		Name: c.PostForm("requesterName"),
-	}
-
-	destination := entity.Destination{
-		Building: c.PostForm("building"),
-	}
-
-	equipment := entity.Equipment{
-		Name: c.PostForm("equipmentName"),
-	}
-
-	jobC.JobUseCase.ExecuteNewJob(jobC.Uow, jobName, requester, destination, equipment)
-
+func (jobController *JobController) CreateNewJob(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+	// ...
 }
