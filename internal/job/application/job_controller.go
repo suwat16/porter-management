2a9b7eaa6ab582f9762095cd1dbcdc35fefd037f
suwat16 <suwat16128@gmail.com>
2024-02-19 21:06:57 +0700

    refactor: 💡 push to job exchange and queue

--- internal/job/application/job_controller.go
+++ internal/job/application/job_controller.go
@@ -15,11 +15,32 @@ func NewJobController(gin *gin.Engine, ch *amqp.Channel) *JobController {
 }
 
 func (jobController *JobController) RegisterRoutes() {
+	//Exchange Declare
+	err := jobController.ch.ExchangeDeclare("job", "direct", true, false, false, false, nil)
+	if err != nil {
+		panic(err)
+	}
+
 	jobController.gin.GET("/job", jobController.GetJobs)
 }
 
 func (jobController *JobController) GetJobs(c *gin.Context) {
+	// Publish a message to the exchange job with the routing key create-job
+
+	// Message is object
+	message := `{"name": "Job 1", "description": "This is job 1"}`
+
+	// Publish a message to the exchange job with the routing key create-job
+	err := jobController.ch.Publish("job", "create-job", false, false, amqp.Publishing{
+		ContentType: "application/json",
+		Body:        []byte(message),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
 	c.JSON(200, gin.H{
-		"message": "pong",
+		"message": "Hello World",
 	})
 }
