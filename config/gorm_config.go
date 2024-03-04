71cc611c0723154e7011f240479a241aa2b353a7
suwat16 <suwat16128@gmail.com>
2024-03-04 20:57:02 +0700

    refactor: 💡 create gorm config file

---
+++ config/gorm_config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type GormConfig struct {
+	host     string
+	user     string
+	password string
+	dbname   string
+}
+
+func NewGormConfig(host, user, password, dbname string) (*gorm.DB, error) {
+	gormConfig := &GormConfig{
+		host:     host,
+		user:     user,
+		password: password,
+		dbname:   dbname,
+	}
+	return gormConfig.Init()
+}
+
+func (g *GormConfig) GetDns() string {
+	return "host=" + g.host + " user=" + g.user + " password=" + g.password + " dbname=" + g.dbname + " port=5432 sslmode=disable TimeZone=Asia/Bangkok"
+}
+
+func (g *GormConfig) Init() (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(g.GetDns()), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	fmt.Println("Connection Opened to Database")
+	return db, nil
+}
